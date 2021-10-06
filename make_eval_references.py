cfa21db6f2a049a73cc331db7552732147f6d4b0
john lee <inno314159@gmail.com>
2021-10-06 11:54:44 +0900

    201006-1

--- make_eval_references.py
+++ make_eval_references.py
@@ -7,6 +7,7 @@ from datetime import timedelta
 
 from utils import count_data
 from decoding import make_html_safe
+from decode_full_model import for_cnn
 
 try:
     DATA_DIR = os.environ['DATA']
@@ -19,13 +20,18 @@ def dump(split):
     print('start processing {} split...'.format(split))
     data_dir = join(DATA_DIR, split)
     dump_dir = join(DATA_DIR, 'refs', split)
-    n_data = count_data(data_dir)
-    for i in range(n_data):
+
+    from glob import glob
+    f_list = glob(data_dir+"/*")
+    n_data = len(f_list)
+    # n_data = count_data(data_dir)
+    #for i in range(n_data):
+    for i,fn in enumerate(f_list):
         print('processing {}/{} ({:.2f}%%)\r'.format(i, n_data, 100*i/n_data),
               end='')
-        with open(join(data_dir, '{}.json'.format(i))) as f:
+        with open(join(data_dir, '{}'.format(fn.split('/')[-1]))) as f:
             data = json.loads(f.read())
-        abs_sents = data['abstract']
+        abs_sents = [for_cnn(''.join(s.split()).strip()) for s in data['abstract']]
         with open(join(dump_dir, '{}.ref'.format(i)), 'w') as f:
             f.write(make_html_safe('\n'.join(abs_sents)))
     print('finished in {}'.format(timedelta(seconds=time()-start)))
